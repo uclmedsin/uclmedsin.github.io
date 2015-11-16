b6386f783d918a130a7c7c26da9688835e15abbf
jazminask <jasmin_ask@hotmail.co.uk>
2015-11-16 20:35:31 +0000

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,8 +1,9 @@
-<script> 
-  $(function() {
+<script src="coding_course/js-folder/jquery.js"></script> 
+  <script> 
+    $(function() {
     
-  $"("#panel1").hide(300).show(1000); 
-  });
-  
-</script>
+      $"("#panel1").hide(300).show(1000); 
+    });
+    
+  </script>
 
